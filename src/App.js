63a8df5601e0535a6d65861999ccd0eba905ede3
Niv Sardi <xaiki@evilgiggle.com>
2018-07-22 16:09:26 -0300

    indent
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/App.js
+++ src/App.js
@@ -11,15 +11,14 @@ import logo from './img/seja-ninja.svg'
 
 const questions = [
   {key: 'name', title: 'Como você se chama? '},
-  {key: 'email', title: 'Qual seu email?'},
+  //  {key: 'email', title: 'Qual seu email?'},
   {key: 'tel', title: 'Qual seu telefone? '},
   {key: 'city', title: 'Qual cidade, estado, país você mora?'},
-  {key: 'profesion', title: 'O que você faz da vida?'},
+  {key: 'activity', title: 'O que você faz da vida?'},
   {key: 'motivation', title: 'Pq vc quer ser NINJA?'},
   {key: 'skills', title: 'Com o que você colaboraria?'},
   {key: 'instagram', title: 'Compartilhe seu instagram'}
 ]
-
 
 class Stepper extends React.PureComponent {
   constructor () {
@@ -53,14 +52,30 @@ const FormWrapper = ({next, ...props}) => (
   />
 )
 
-const SendData = ({next, args}) => {
-  const { name, city, area, email, phone } = args[1]
+const formMapping = {
+  name: 'entry.276763906',
+  email: 'entry.2062246402',
+  tel: 'entry.1698681350',
+  city: 'entry.884182261',
+  activity: 'entry.1340129654',
+  motivation: 'entry.322372410',
+  skills: 'entry.919281481',
+  instagram: 'entry.1348441891'
+}
 
+const SendData = ({next, args}) => {
+  const [element, results] = args
+  const { name, city, area, email, phone } = results
+  const params = Object.keys(results)
+    .map(key => `${formMapping[key]}=${results[key]}`)
+    .join('&')
+
+  const url = `https://docs.google.com/forms/d/14F27ai-E3gaBKR3ca2KFcLiU-wrBEE8wDrsAoNa328o/viewform?${params}`
   return (
     <h1>VALEU <em>{name}</em> de <em>{city}</em>, favor de
-    <button className='pink'>
-            Confirmar
-      </button>
+      <button className='pink' onClick={window.open(url)}>
+          Confirmar
+    </button>
     </h1>
   )
 }
@@ -68,7 +83,7 @@ const SendData = ({next, args}) => {
 const Details = () => (
   <div className='flex'>
     <h1>Preencha seus dados para que possamos te conhecer
-            melhor e entrar em contato</h1>
+        melhor e entrar em contato</h1>
     <Stepper>
       <FormWrapper />
       <SendData />
